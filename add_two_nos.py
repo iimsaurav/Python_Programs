435b062e42f3c868fa421a5c530c9d9d1629d0ed
iimsaurav <mr.saurav98@gmail.com>
2020-10-26 14:05:45 +0530

    modified

--- add_two_nos.py
+++ add_two_nos.py
@@ -1,10 +1,11 @@
-# This program adds two numbers
+# This program adds two numbers by taking inputs from user
 
-num1 = 10.5
-num2 = 6.3
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
 
 # Add two numbers
-sum = num1 + num2
+sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
