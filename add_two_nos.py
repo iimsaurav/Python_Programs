2b3f66fd1c7b0d68d4e1928f3be8b7b7bbb36891
iimsaurav <mr.saurav98@gmail.com>
2022-08-27 16:31:26 +0530

    initial commit

--- add_two_nos.py
+++ add_two_nos.py
@@ -5,7 +5,7 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 # Add two numbers
-sum = float(num1) + float(num2)
+sum_of_two_no = float(num1) + float(num2)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum_of_two_no))
